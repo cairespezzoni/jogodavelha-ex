68bfde3ce942a55da93fa5839978950803030327
cairespezzoni <caires.pezzoni@gmail.com>
2017-04-17 08:52:40 -0300

    Jogo funcional e implementado. configurando para rede

--- js/controladores/socketCtrl.js
+++ js/controladores/socketCtrl.js
@@ -35,7 +35,8 @@ app.controller('socketCtrl', ['$scope', function ($scope) {
         },
 
         iniciaNovoJogo: function(data) {
-            $scope.Jogo[$scope.Jogo.meuPapel].jogoContagem(data);
+            console.log('Começando novo jogo - ' + $scope.Jogo.meuPapel);
+            $scope.Jogo[$scope.Jogo.meuPapel].jogoSorteio(data);
         },
 
         hostChecaJogada: function(data) {
@@ -87,43 +88,46 @@ app.controller('socketCtrl', ['$scope', function ($scope) {
 
             mostraTelaNovoJogo: function() {
                 // Exibir tela de jogo novo
-                console.log('mostraTelaNovoJogoHost');
                 $scope.pagina = "./html/novoJogo.html";
 
                 // Exibir URL e Jogo.gameId para que o outro jogador possa pegar o código
                 $scope.Jogo.Host.url = window.location.href;
             },
 
-            atualizaTelaEspera: function(data) {
+            
+            atualizaTelaEspera : function(data) {
                 console.log("Jogador " + data.jogadorNome + " entrou no jogo!");
                 
                 if ($scope.Jogo.Host.jogoNovo) {
                     $scope.Jogo.Host.mostraTelaNovoJogo();
                 };
 
-                // Adicionar novo jogador na tela de espera
-                $scope.Jogo.Host.jogadores.push(data);
-                //console.log($scope.Jogo.Host.jogadores);
-                $scope.$apply();
+                if( $scope.Jogo.Host.numJogadoresNaSala < 2) {
+                    // Adicionar novo jogador na tela de espera
+                    $scope.Jogo.Host.jogadores.push(data);
+                    console.log($scope.Jogo.Host.jogadores);
+
+                    $scope.Jogo.Host.numJogadoresNaSala += 1;
+
+                } else if( $scope.Jogo.Host.numJogadoresNaSala === 2) {
+                    IO.socket.emit('hostSalaCheia', $scope.Jogo.gameId);
+                } else {
+                    console.log('Sala cheia!!!!!');
+                };
 
                 $scope.Jogo.Host.numJogadoresNaSala += 1;
-
-                if( $scope.Jogo.Host.numJogadoresNaSala === 2) {
-                    IO.socket.emit('hostSalaCheia', $scope.Jogo.gameId);
-                };
+                $scope.$apply();
             },
 
-            jogoContagem : function() {
+            jogoSorteio : function() {
 
                 // Mostrar nova tela  de contagem para início
+                $scope.pagina = './html/grid.html';
+                $scope.Jogo.sorteio = true;
+                $scope.$apply();
 
                 // Começar contagem na tela (talvez)
-                $scope.Jogo.jogoContagem($scope.tempoContagem, 5, function() {
-                    IO.socket.emit('hostContagemTerminada', $scope.Jogo.gameId);
-                });
-
-                // Exibir nomes dos jogadores na tela
-                // $scope.Jogo.Host.players[X].jogadorNome
+                //IO.socket.emit('hostContagemTerminada', $scope.Jogo.gameId);
 
                 // Cara ou coroa deve ser executado aqui ou logo antes deste método
             },
@@ -209,26 +213,35 @@ app.controller('socketCtrl', ['$scope', function ($scope) {
             },
 
             atualizaTelaEspera: function(data) {
-                if (IO.socket.sessionid === data.meuSocketId) {
+                console.log(IO.socket.id + ' = ' + data.meuSocketId);
+                if (IO.socket.id === data.meuSocketId) {
                     $scope.Jogo.meuPapel = 'Player';
                     $scope.Jogo.gameId = data.gameId;
 
                     // Exibir mensagem de "Uniu ao jogo, aguardando novo jogo começar"
-                }
+                    $scope.Jogo.listaEspera = true;
+                    console.log('lista de espera: ' + $scope.Jogo.listaEspera);
+                }   
             },
 
-            jogoContagem: function(hostData) {
+            jogoSorteio : function(hostData) {
+                $scope.pagina = '/html/grid.html';
+                $scope.Jogo.Player.hostSocketId = hostData.meuSocketId;
+                $scope.$apply();
+            },
+
+            /*contagem: function(hostData) {
                 // O $scope.Jogo é iniciado aqui!!
                 
                 $scope.Jogo.Player.hostSocketId = hostData.meuSocketId;
                 // mostrar mensagem de "Preparado?"
-            },
+            },*/
 
             endGame: function() {
                 // mostrar mensagem de fim de jogo com opções de
                 // Reiniciar ou Quitar
             }
-        },
+        }/*,
 
         contagem: function(tempoIni, callback) {
             
@@ -245,7 +258,8 @@ app.controller('socketCtrl', ['$scope', function ($scope) {
                     return;
                 }
             }
-        }
+        }*/
+
     };
 
     IO.init();
--- js/controladores/veiaCtrl.js
+++ js/controladores/veiaCtrl.js
@@ -48,12 +48,12 @@ app.controller('veiaCtrl', ['$scope', function($scope) {
         for (var condicao = 0; condicao < _vitoria.length; condicao++) {
             for (var linha = 0; linha < _vitoria[condicao].length; linha++) {
                 for (var coluna = 0; coluna < _vitoria[condicao][linha].length; coluna++) {
-                    console.log("Condicao: " + condicao + ", Linha: " + linha + ", Coluna: " + coluna);
-                    console.log($scope.cerquilha[linha][coluna] + " compara com " + _vitoria[condicao][linha][coluna]);
+                    //console.log("Condicao: " + condicao + ", Linha: " + linha + ", Coluna: " + coluna);
+                    //console.log($scope.cerquilha[linha][coluna] + " compara com " + _vitoria[condicao][linha][coluna]);
                     if ($scope.cerquilha[linha][coluna] === _vitoria[condicao][linha][coluna] &&
                         $scope.cerquilha[linha][coluna] === n) {
                             _venceu++;
-                            console.log($scope.cerquilha[linha][coluna] === _vitoria[condicao][linha][coluna]);
+                            //console.log($scope.cerquilha[linha][coluna] === _vitoria[condicao][linha][coluna]);
                     }
                 }
             }
@@ -81,23 +81,23 @@ app.controller('veiaCtrl', ['$scope', function($scope) {
     $scope.muda = function(linha, coluna) {
         if (!$scope.cerquilha[linha][coluna]) {
             
-            if ($scope.jogador[0].ativo && !$scope.jogador[1].ativo) {
-                $scope.jogador[0].ativo = false;
-                $scope.jogador[1].ativo = true;
+            if ($scope.jogadores[0].ativo && !$scope.jogadores[1].ativo) {
+                $scope.jogadores[0].ativo = false;
+                $scope.jogadores[1].ativo = true;
                 $scope.cerquilha[linha][coluna] = 1;
                 $scope.estilo[linha][coluna] = "jogador1";
                 if (checaVitoria(1)) {
                     $scope.vitoria = "Jogador 1";
-                    $scope.jogador[1].ativo = false;
+                    $scope.jogadores[1].ativo = false;
                 };
-            } else if (!$scope.jogador[0].ativo && $scope.jogador[1].ativo) {
-                $scope.jogador[0].ativo = true;
-                $scope.jogador[1].ativo = false;
+            } else if (!$scope.jogadores[0].ativo && $scope.jogadores[1].ativo) {
+                $scope.jogadores[0].ativo = true;
+                $scope.jogadores[1].ativo = false;
                 $scope.cerquilha[linha][coluna] = -1;
                 $scope.estilo[linha][coluna] = "jogador2";
                 if (checaVitoria(-1)) {
                     $scope.vitoria = "Jogador 2";
-                    $scope.jogador[0].ativo = false;
+                    $scope.jogadores[0].ativo = false;
                 };
             };
         };
